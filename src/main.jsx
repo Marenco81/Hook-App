5311417151cef12e43267bc3c903fa53ef029cc0
Alvaro Marenco <alvaromarencom@gmail.com>
2024-10-31 15:22:13 -0600

    Implementando useRef en FocusScreen

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import { StrictMode, } from 'react'
 import { createRoot, ReactDOM } from 'react-dom/client'
 import './index.css'
+import { FocusScreen } from './04-useRef/FocusScreen'
 // import { SimpleForm } from './02-useEffect/SimpleForm'
 // import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
-import { MultipleCustomHoooks } from './03-examples/MultipleCustomHoooks'
+// import { MultipleCustomHoooks } from './03-examples/MultipleCustomHoooks'
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 // import { CounterApp } from './01-useState/CounterApp'
 // import { HooksApp } from './HooksApp'
@@ -11,6 +12,6 @@ import { MultipleCustomHoooks } from './03-examples/MultipleCustomHoooks'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <MultipleCustomHoooks />
+    <FocusScreen />
   </StrictMode>,
 )
