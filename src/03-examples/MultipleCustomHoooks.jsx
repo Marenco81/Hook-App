634dbfd7de04f43ef6c2930c1fc8b917652cb166
Alvaro Marenco <alvaromarencom@gmail.com>
2024-10-30 18:17:43 -0600

    Creando MultipleCustomHook

--- src/03-examples/MultipleCustomHoooks.jsx
+++ src/03-examples/MultipleCustomHoooks.jsx
@@ -1,18 +1,53 @@
 import React from 'react'
-import { useFetch } from '../hooks'
+import { useCounter, useFetch } from '../hooks'
+import { LoadingMessage } from './LoadingMessage';
+import { PokemonCard } from './PokemonCard';
 
 export const MultipleCustomHoooks = () => {
 
-    const {data, hasError, isLoading} = useFetch('https://pokeapi.co/api/v2/pokemon/5');
+    const {counter, increment, decrement} = useCounter(1);
 
+    const {data, hasError, isLoading} = useFetch(`https://pokeapi.co/api/v2/pokemon/${counter}`);
+    console.log(counter);
 
   return (
     <>
     <h1>Información del Pokemón</h1>
     <hr />
 
+    { 
+    isLoading? 
+    <LoadingMessage/>
+    : (
+        < PokemonCard 
+            id={data.id} 
+            name={data.name}
+            sprites={[
+                data.sprites.front_default,
+                data.sprites.front_shiny,
+                data.sprites.back_default,
+                data.sprites.back_shiny,
+            ]}
+        />
+      ) 
+    }
+    
+
     {/* <pre>{JSON.stringify(data, null, 2)} </pre> */}
     <h3> {data?.name} </h3>
+
+    <button
+        onClick={ () => decrement(1)}
+        className='btn btn-primary mt-2'
+    >
+        Anterior
+    </button>
+    <button
+        onClick={ () => increment(1)}
+        className='btn btn-primary mt-2'
+    >
+        Siguiente
+    </button>
 
     </>
   )
---
+++ src/03-examples/PokemonCard.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable no-unused-vars */
+
+import React from 'react'
+
+export const PokemonCard = ({id, name, sprites = []}) => {
+  return (
+    <section style={{height:200}}>
+        <h2 className='text-capitalized'>#{id} - {name} </h2>
+
+        {/* imagenes */}
+        <div>
+            {
+                sprites.map(sprite => (
+                    <img key={sprite} src={sprite} alt={name} />
+                ))
+            }
+        </div>
+    </section>
+  )
+}
